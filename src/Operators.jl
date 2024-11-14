32ad8544d5e9fcf261ea54d036b70f23046d178c
jmeziere <jmeziere@gmail.com>
2024-11-14 16:47:39 -0700

    add rotations

--- src/Operators.jl
+++ src/Operators.jl
@@ -44,6 +44,55 @@ function operate(er::ER, state)
 end
 
 """
+    EROpt()
+
+Create an object that applies one iteration of Error Reduction (ER)
+to the currently [`Mount`](@ref)ed peak. ER is an iterative projection
+algorithm that enforces two constraints, (1) the modulus constraint
+and (2) the support constraint:
+
+1. When moved to reciprocal space, the reconstructed object must match the diffraction pattern.
+2. The reconstructed object must fully lie within the support.
+
+One iteration of ER first applies the modulus constraint, then the
+support constraint to the object, then returnns.
+
+Gradient descent is an alternate way to view the ER algorithm becausee
+ER is equivalent to gradient descent with a step size of 0.5.
+
+More information about the ER algorithm can be found in [Fienup1978,Marchesini2007](@cite).
+"""
+struct EROpt{T} <: Operator
+    eropt::T
+
+    function EROpt(state)
+        neighbors = CUDA.zeros(Int64, 6, length(state.keepInd))
+        inds = [
+            CartesianIndex(1,0,0), CartesianIndex(0,1,0), CartesianIndex(0,0,1), 
+            CartesianIndex(-1,0,0), CartesianIndex(0,-1,0), CartesianIndex(0,0,-1)
+        ]
+        function myFind(a,b)
+            ret = findfirst(a,b)
+            if ret == nothing
+                return 0
+            end
+            return ret
+        end
+        for i in 1:length(inds)
+            neighs = myFind.(isequal.(state.keepInd .- inds[i]), Ref(state.keepInd))
+            neighbors[i,:] .= neighs
+        end
+        
+        eropt = BcdiTrad.EROpt(neighbors)
+        new{typeof(eropt)}(eropt)
+    end
+end
+
+function operate(eropt::EROpt, state)
+    BcdiTrad.operate(eropt.eropt, state.traditionals[state.currTrad[]])
+end
+
+"""
     HIO(beta)
 
 Create an object that applies an iteration of hybrid input-output (HIO)
@@ -68,14 +117,50 @@ More information about the HIO algorithm can be found in [Fienup1978,Marchesini2
 struct HIO{T} <: Operator
     hio::T
 
-    function HIO(beta)
-        hio = BcdiTrad.HIO(beta)
+    function HIO(beta, state)
+        hio = BcdiTrad.HIO(beta, state.traditionals[state.currTrad[]])
         new{typeof(hio)}(hio)
     end
 end
 
 function operate(hio::HIO, state)
     BcdiTrad.operate(hio.hio, state.traditionals[state.currTrad[]])
+end
+
+"""
+    HIOOpt(beta)
+
+Create an object that applies an iteration of hybrid input-output (HIO)
+to the currently [`Mount`](@ref)ed peak. On the interior of the support,
+HIO is equivalent to applying the modulus constraint as described in the
+[`ER`](@ref) algorithm, and on the exterior of the support, HIO is equal
+to the current reconstruction minus a fraction of the output after applying
+the modulus constraint, that is,
+
+```math
+\\rho_{i+1} = \\begin{cases}
+ER(\\rho_i) & \\rho \\in support \\\\
+\\rho_i - \\beta * ER(\\rho_i) & \\rho \\notin support
+\\end{cases}
+```
+
+Marchesini [Marchesini2007](@cite) has shown that the HIO algorithm is
+equivalent to a mini-max problem.
+
+More information about the HIO algorithm can be found in [Fienup1978,Marchesini2007](@cite).
+"""
+struct HIOOpt{T} <: Operator
+    hioopt::T
+
+    function HIOOpt(alpha, state)
+        neighbors = CUDA.zeros(Int64, 6, length(state.rho))
+        hioopt = BcdiTrad.HIOOpt(alpha, state.traditionals[state.currTrad[]])
+        new{typeof(hioopt)}(hioopt)
+    end
+end
+
+function operate(hioopt::HIOOpt, state)
+    BcdiTrad.operate(hioopt.hioopt, state.traditionals[state.currTrad[]])
 end
 
 """
@@ -93,7 +178,7 @@ struct Shrink{T} <: Operator
     shrink::T
 
     function Shrink(threshold, sigma, state)
-        shrink = BcdiTrad.Shrink(threshold, sigma, state.traditionals[state.currTrad[]])
+        shrink = BcdiTrad.Shrink(threshold, sigma, state.traditionals[state.currTrad[]], state.keepInd)
         new{typeof(shrink)}(shrink)
     end
 end
@@ -115,7 +200,7 @@ struct Center{T} <: Operator
     center::T
 
     function Center(state)
-        center = BcdiTrad.Center(state.traditionals[state.currTrad[]])
+        center = BcdiTrad.Center(state.traditionals[state.currTrad[]], state.keepInd)
         new{typeof(center)}(center)
     end
 end
@@ -152,14 +237,15 @@ random and the current solution is projected out to this peak.
 
 The paper that describes this algorithm is currently in submission.
 """
-struct Mount <: Operator
+struct Mount{I} <: Operator
     beta::Float64
-    xArr::CuArray{Float64, 3, CUDA.Mem.DeviceBuffer}
+    xArr::CuArray{Float64, I, CUDA.Mem.DeviceBuffer}
     AInv::CuArray{Float64, 2, CUDA.Mem.DeviceBuffer}
 
     function Mount(beta, state, primitiveRecipLattice)
-        xArr = CUDA.zeros(Float64, size(state.rho))
-        new(beta, xArr, primitiveRecipLattice)
+        xArr = CUDA.zeros(Float64, size(state.keepInd))
+
+        new{ndims(xArr)}(beta, xArr, primitiveRecipLattice)
     end
 end
 
@@ -167,12 +253,13 @@ function operate(mount::Mount, state::State)
     beta = mount.beta
     tct = state.traditionals[state.currTrad[]]
     currG = state.gVecs[state.currTrad[]]
+    ki = state.keepInd
     xArr = mount.xArr
 
     @views rspMul = 
         mapreduce(
             (re, rh, sup)-> sup ? abs(re) * rh : 0.0, +, 
-            tct.realSpace, state.rho, 
+            tct.realSpace, state.rho[ki], 
             tct.support
         ) / 
         mapreduce(
@@ -180,58 +267,73 @@ function operate(mount::Mount, state::State)
             tct.realSpace, 
             tct.support
         )
-    if reduce(+, state.rho) < 1e-6
+    @views if reduce(+, state.rho[ki]) < 1e-6
         beta = 1.0
         rspMul = 1.0
     end
 
-    tct.realSpace .*= tct.support .* rspMul
-    state.rho .*= (1.0 .- beta) .* tct.support
-    state.rho .+= beta .* abs.(tct.realSpace)
-    state.ux .*= tct.support
-    state.uy .*= tct.support
-    state.uz .*= tct.support
+    @views rho = state.rho[ki]
+    @views ux = state.ux[ki]
+    @views uy = state.uy[ki]
+    @views uz = state.uz[ki]
 
-    
+    tct.realSpace .*= tct.support .* rspMul
+    rho .*= (1.0 .- beta) .* tct.support
+    rho .+= beta .* abs.(tct.realSpace)
+    ux .*= tct.support
+    uy .*= tct.support
+    uz .*= tct.support
+
     xArr .= beta .* minDiffAngle.(
         tct.realSpace,
-        currG[1] .* state.ux .+ currG[2] .* state.uy .+ currG[3] .* state.uz
+        .-(currG[1] .* ux .+ currG[2] .* uy .+ currG[3] .* uz)
     ) ./ dot(currG, currG)
     @views xArr .-= median(xArr[abs.(tct.realSpace) .> 1e-6])
     xArr .*= abs.(tct.realSpace) .> 1e-6
 
-    state.ux .+= xArr .* currG[1]
-    state.uy .+= xArr .* currG[2]
-    state.uz .+= xArr .* currG[3]
+    ux .-= xArr .* currG[1]
+    uy .-= xArr .* currG[2]
+    uz .-= xArr .* currG[3]
 
+    @views sux = state.ux[ki[tct.support]]
+    @views suy = state.uy[ki[tct.support]]
+    @views suz = state.uz[ki[tct.support]]
     allU = CUDA.zeros(Float64, 3, reduce(+, tct.support))
-    allU[1,:] .= state.ux[tct.support]
-    allU[2,:] .= state.uy[tct.support]
-    allU[3,:] .= state.uz[tct.support]
+    allU[1,:] .= sux
+    allU[2,:] .= suy
+    allU[3,:] .= suz
 
     B = floor.(Int64, (mount.AInv * allU) ./ (2 .* pi) .+ 0.5)
     allU .-= 2 .* pi .* (mount.AInv \ B)
-    state.ux[tct.support] .= allU[1,:]
-    state.uy[tct.support] .= allU[2,:]
-    state.uz[tct.support] .= allU[3,:]
+    @views sux .= allU[1,:]
+    @views suy .= allU[2,:]
+    @views suz .= allU[3,:]
     
     state.currTrad[] = rand(1:length(state.traditionals))
+    ct = state.currTrad[]
     tct = state.traditionals[state.currTrad[]]
     currG = state.gVecs[state.currTrad[]]
-
-    tct.realSpace .= state.rho .* exp.(1im .* (
-         currG[1] .* state.ux .+ currG[2] .* state.uy .+ currG[3] .* state.uz
+    tct.realSpace .= rho .* exp.(.-1im .* (
+         currG[1] .* ux .+ currG[2] .* uy .+ currG[3] .* uz
     ))
+
+    if state.highStrain && state.rotations == nothing
+        BcdiCore.setpts!(tct.core, state.xPos[1] .+ ux, state.yPos[1] .+ uy, state.zPos[1] .+ uz, true)
+    elseif state.highStrain
+        BcdiCore.setpts!(tct.core, state.xPos[ct] .+ ux, state.yPos[ct] .+ uy, state.zPos[ct] .+ uz, true)
+    elseif state.rotations != nothing
+        BcdiCore.setpts!(tct.core, state.xPos[ct], state.yPos[ct], state.zPos[ct], true)
+    end
     tct.core.plan * tct.realSpace
 
     tct.realSpace .*= 
         mapreduce(
             (i,rsp,sup) -> sup ? sqrt(i) * abs(rsp) : 0.0, +, 
-            tct.core.intens, tct.core.plan.recipSpace, tct.core.recSupport, init=0.0
-        ) /
+            tct.core.intens, tct.core.plan.recipSpace, tct.core.recSupport, dims=(1,2,3)
+        ) ./
         mapreduce(
             (rsp,sup) -> sup ? abs2(rsp) : 0.0, +, 
-            tct.core.plan.recipSpace, tct.core.recSupport, init=0.0
+            tct.core.plan.recipSpace, tct.core.recSupport, dims=(1,2,3)
         )
 end
 
--- src/State.jl
+++ src/State.jl
@@ -12,7 +12,7 @@ zero frequency). If the support is not passed in, an initial guess of the suppor
 is created by taking an IFFT of the intensities and including everything above
 0.1 times the maximum value.
 """
-struct State
+struct State{T,I}
     rho::CuArray{Float64, 3, CUDA.Mem.DeviceBuffer}
     ux::CuArray{Float64, 3, CUDA.Mem.DeviceBuffer}
     uy::CuArray{Float64, 3, CUDA.Mem.DeviceBuffer}
@@ -23,36 +23,102 @@ struct State
     traditionals::Vector{BcdiTrad.State}
     currTrad::Base.RefValue{Int64}
     gVecs::Vector{Vector{Float64}}
+    rotations::T
+    highStrain::Bool
+    xPos::Vector{CuArray{Float64, 1, CUDA.Mem.DeviceBuffer}}
+    yPos::Vector{CuArray{Float64, 1, CUDA.Mem.DeviceBuffer}}
+    zPos::Vector{CuArray{Float64, 1, CUDA.Mem.DeviceBuffer}}
+    keepInd::CuArray{CartesianIndex{3}, I, CUDA.Mem.DeviceBuffer}
 
-    function State(intens, gVecs, recSupports)
-#        support = CUDA.ones(Bool, size(intens[1]))
-#        for i in 1:length(intens)
-#            invInt = CUFFT.ifft(CuArray{Float64, 3, CUDA.Mem.DeviceBuffer}(intens[i]))
-#            support .= support .&& (abs.(invInt) .> 0.1 * maximum(abs.(invInt)))
-#        end
-        invInt = CUFFT.ifft(CuArray{Float64, 3, CUDA.Mem.DeviceBuffer}(intens[1]))
-        support = abs.(invInt) .> 0.1 * maximum(abs.(invInt))
-
-        State(intens, gVecs, recSupports, support)
-    end
-
-    function State(intens, gVecs, recSupports, support)
+    function State(intens, gVecs, recSupports; rotations=nothing, highStrain=false, truncRecSupport=true)
         s = size(intens[1])
+        n = rotations == nothing && !highStrain ? size(intens[1]) : reduce(*, size(intens[1]))
         rho = CUDA.zeros(Float64, s)
         ux = CUDA.zeros(Float64, s)
         uy = CUDA.zeros(Float64, s)
         uz = CUDA.zeros(Float64, s)
+        support = CUDA.zeros(Bool, n)
 
-        emptyCore = BcdiCore.TradState("L2", false, CUDA.zeros(Float64, s), CUDA.zeros(Float64, s), CUDA.zeros(Float64, s))
-        traditionals = [BcdiTrad.State(intens[i], recSupports[i], support, emptyCore) for i in 1:length(intens)]
+        emptyCore = BcdiCore.TradState("L2", false, CUDA.zeros(Float64, n), CUDA.zeros(Float64, s), CUDA.zeros(Float64, s))
+        traditionals = [BcdiTrad.State(intens[i], recSupports[i], support, emptyCore, truncRecSupport) for i in 1:length(intens)]
         currTrad = Ref(rand(1:length(intens)))
 
         xArr = CUDA.zeros(Float64, s)
         yArr = CUDA.zeros(Float64, s)
         zArr = CUDA.zeros(Float64, s)
-        new(
+
+        xPos = []
+        yPos = []
+        zPos = []
+        if rotations != nothing || highStrain
+            baseX = zeros(Float64, size(xArr))
+            baseY = zeros(Float64, size(xArr))
+            baseZ = zeros(Float64, size(xArr))
+            for i in 1:size(xArr, 1)
+                for j in 1:size(xArr, 2)
+                    for k in 1:size(xArr, 3)
+                        baseX[i,j,k] = 2*pi*(i-1)/size(xArr, 1)-pi
+                        baseY[i,j,k] = 2*pi*(j-1)/size(xArr, 2)-pi
+                        baseZ[i,j,k] = 2*pi*(k-1)/size(xArr, 3)-pi
+                    end
+                end
+            end
+            baseCuX = CuArray{Float64}(baseX)
+            baseCuY = CuArray{Float64}(baseY)
+            baseCuZ = CuArray{Float64}(baseZ)
+            baseX = vec(baseX)
+            baseY = vec(baseY)
+            baseZ = vec(baseZ)
+
+            cpuXPos = []
+            cpuYPos = []
+            cpuZPos = []
+            if rotations != nothing
+                deleteInd = []
+                for i in 1:length(rotations)
+                    rot = transpose(rotations[i])
+                    x = rot[1,1] .* baseX .+ rot[1,2] .* baseY .+ rot[1,3] .* baseZ
+                    y = rot[2,1] .* baseX .+ rot[2,2] .* baseY .+ rot[2,3] .* baseZ
+                    z = rot[3,1] .* baseX .+ rot[3,2] .* baseY .+ rot[3,3] .* baseZ
+                    
+                    push!(cpuXPos, x)
+                    push!(cpuYPos, y)
+                    push!(cpuZPos, z)
+                    append!(deleteInd, findall(x .> pi .|| x .< -pi .|| y .> pi .|| y .< -pi .|| z .> pi .|| z .< -pi))
+                end
+                deleteInd = sort(unique(deleteInd))
+                keepInd = deleteat!(collect(vec(CartesianIndices(s))),deleteInd)
+                for i in 1:length(rotations)
+                    deleteat!(cpuXPos[i], deleteInd)
+                    deleteat!(cpuYPos[i], deleteInd)
+                    deleteat!(cpuZPos[i], deleteInd)
+                    push!(xPos, CuArray{Float64}(cpuXPos[i]))
+                    push!(yPos, CuArray{Float64}(cpuYPos[i]))
+                    push!(zPos, CuArray{Float64}(cpuZPos[i]))
+                end
+                BcdiCore.setpts!(traditionals[currTrad[]].core, xPos[currTrad[]], yPos[currTrad[]], zPos[currTrad[]], true)
+                resize!(traditionals[currTrad[]].support, length(xPos[1]))
+                resize!(traditionals[currTrad[]].realSpace, length(xPos[1]))
+            else
+                baseX = CuArray{Float64}(baseX)
+                baseY = CuArray{Float64}(baseY)
+                baseZ = CuArray{Float64}(baseZ)
+                BcdiCore.setpts!(traditionals[currTrad[]].core, baseX, baseY, baseZ, true)
+                push!(xPos, baseX)
+                push!(yPos, baseY)
+                push!(zPos, baseZ)
+                keepInd = collect(vec(CartesianIndices(s)))
+            end
+        else
+            keepInd = collect(CartesianIndices(s))
+        end
+        BcdiTrad.initializeState(traditionals[currTrad[]].support, traditionals[currTrad[]].core)
+
+        new{typeof(rotations),ndims(keepInd)}(
             rho,ux,uy,uz,xArr,yArr,zArr,
-            traditionals,currTrad,gVecs
+            traditionals,currTrad,gVecs,
+            rotations, highStrain, xPos, yPos, zPos,
+            keepInd
         )
     end
 end
